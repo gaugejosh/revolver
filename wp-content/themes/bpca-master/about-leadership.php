0b5d6458aedbb2b2ec0a5e396bb085b1f0fdf11a
Joshua Romero <josh@gaugeinteractive.com>
2015-02-19 22:38:00 -0500

    Removing some plugins and more styling changes.

--- wp-content/themes/bpca-master/about-leadership.php
+++ wp-content/themes/bpca-master/about-leadership.php
@@ -8,34 +8,40 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<div class="breadcrumbs" xmlns:v="http://rdf.data-vocabulary.org/#">
-				<?php if (function_exists('bcn_display') && ! is_home())
-				{
-					bcn_display();
-				} ?>
+			<div class="breadcrumbs">
+				<?php if (function_exists('bcn_display') && ! is_home()) bcn_display(); ?>
 			</div>
 
 			<div class="about-leaders">
 				<ul class="leaders">
 					<?php
 						$args = array(
-						'post_type'      => 'leaders',
-						'posts_per_page' => - 1,
-						'meta_key'       => 'leader_order',
-						'orderby'        => 'meta_value_num',
-						'order'          => 'ASC'
+							'post_type'      => 'leaders',
+							'posts_per_page' => - 1,
+							'meta_key'       => 'leader_order',
+							'orderby'        => 'meta_value_num',
+							'order'          => 'ASC'
 						);
 
-						// start the loop
+						// instantiate new WP_Query object
 						$loop = new WP_Query($args);
+
+						// start ze loop!
 						while ($loop->have_posts()) : $loop->the_post();
 					?>
 						<li class="leader">
-							<div class="leader-info">
+							<div class="leader-content">
+								<div class="leader-info">
+									<ul class="leader-info-list">
+										<li><h2><?php the_title() ?></h2></li>
+										<li><?php the_field('position_title') ?></li>
+										<li><?php the_field('confirmed_on') ?></li>
+									</ul>
+								</div><!-- .leader-info -->
 								<div class="leader-image">
 									<?php the_post_thumbnail() ?>
 								</div><!-- .leader-image -->
-							</div><!-- .leader-info -->
+							</div><!-- .leader-content -->
 						</li>
 					<?php endwhile; ?>
 				</ul>
